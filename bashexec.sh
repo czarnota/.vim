4b5f256a24b5baf0572f0b4b80bf27cf2abb4635
Przemyslaw Czarnota <x2n404@gmail.com>
2025-06-04 09:51:37 +0000

    bashexec: add comment execution

--- bashexec.sh
+++ bashexec.sh
@@ -2,6 +2,26 @@
 
 # Buffer all input so that executed script can access it
 export VIM_CONTENTS=$(cat)
+export VIM_CONTENTS_RAW="$VIM_CONTENTS"
 
-# Pass input to bash
+if [[ "$VIM_CONTENTS" == \#!bash* ]]; then
+    VIM_CONTENTS="$(sed -n -E 's/^#!bash//p;s/^# {0,1}//p' <<< "$VIM_CONTENTS")"
+    sed -n -E '/^#/p' <<< "$VIM_CONTENTS_RAW"
+elif [[ "$VIM_CONTENTS" == \#\ !bash* ]]; then
+    VIM_CONTENTS="$(sed -n -E 's/^# !bash//p;s/^# {0,1}//p' <<< "$VIM_CONTENTS")"
+    sed -n -E '/^#/p' <<< "$VIM_CONTENTS_RAW"
+elif [[ "$VIM_CONTENTS" == //\!bash* ]]; then
+    VIM_CONTENTS="$(sed -n -E 's/^\/\/!bash//p;s/^\/\/ {0,1}//p' <<< "$VIM_CONTENTS")"
+    sed -n -E '/^\/\//p' <<< "$VIM_CONTENTS_RAW"
+elif [[ "$VIM_CONTENTS" == //\ !bash* ]]; then
+    VIM_CONTENTS="$(sed -n -E 's/^\/\/ !bash//p;s/^\/\/ {0,1}//p' <<< "$VIM_CONTENTS")"
+    sed -n -E '/^\/\//p' <<< "$VIM_CONTENTS_RAW"
+elif [[ "$VIM_CONTENTS" == /\*!bash* ]]; then
+    VIM_CONTENTS="$(sed -n -E 's/^\/\*!bash//p;s/^(\/\*| \*| \*\/)//p' <<< "$VIM_CONTENTS")"
+    sed -n -E '/^(\/\*| \*| \*\/)/p' <<< "$VIM_CONTENTS_RAW"
+elif [[ "$VIM_CONTENTS" == /\*\ !bash* ]]; then
+    VIM_CONTENTS="$(sed -n -E 's/^\/\* !bash//p;s/^(\/\*| \*| \*\/)//p' <<< "$VIM_CONTENTS")"
+    sed -n -E '/^(\/\*| \*| \*\/)/p' <<< "$VIM_CONTENTS_RAW"
+fi
+
 bash <<< "$VIM_CONTENTS"
