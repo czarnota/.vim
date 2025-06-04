1b944191d3629bf6a3bb8f6578c55ee01bf24978
Przemyslaw Czarnota <x2n404@gmail.com>
2025-06-04 12:55:15 +0000

    bashexec: execute any code in comment execution

--- bashexec.sh
+++ bashexec.sh
@@ -1,27 +1,76 @@
 #!/usr/bin/env bash
 
 # Buffer all input so that executed script can access it
-export VIM_CONTENTS=$(cat)
-export VIM_CONTENTS_RAW="$VIM_CONTENTS"
+export VIM_CONTENTS="$(cat)"
 
-if [[ "$VIM_CONTENTS" == \#!bash* ]]; then
-    VIM_CONTENTS="$(sed -n -E 's/^#!bash//p;s/^# {0,1}//p' <<< "$VIM_CONTENTS")"
-    sed -n -E '/^#/p' <<< "$VIM_CONTENTS_RAW"
-elif [[ "$VIM_CONTENTS" == \#\ !bash* ]]; then
-    VIM_CONTENTS="$(sed -n -E 's/^# !bash//p;s/^# {0,1}//p' <<< "$VIM_CONTENTS")"
-    sed -n -E '/^#/p' <<< "$VIM_CONTENTS_RAW"
-elif [[ "$VIM_CONTENTS" == //\!bash* ]]; then
-    VIM_CONTENTS="$(sed -n -E 's/^\/\/!bash//p;s/^\/\/ {0,1}//p' <<< "$VIM_CONTENTS")"
-    sed -n -E '/^\/\//p' <<< "$VIM_CONTENTS_RAW"
-elif [[ "$VIM_CONTENTS" == //\ !bash* ]]; then
-    VIM_CONTENTS="$(sed -n -E 's/^\/\/ !bash//p;s/^\/\/ {0,1}//p' <<< "$VIM_CONTENTS")"
-    sed -n -E '/^\/\//p' <<< "$VIM_CONTENTS_RAW"
-elif [[ "$VIM_CONTENTS" == /\*!bash* ]]; then
-    VIM_CONTENTS="$(sed -n -E 's/^\/\*!bash//p;s/^(\/\*| \*| \*\/)//p' <<< "$VIM_CONTENTS")"
-    sed -n -E '/^(\/\*| \*| \*\/)/p' <<< "$VIM_CONTENTS_RAW"
-elif [[ "$VIM_CONTENTS" == /\*\ !bash* ]]; then
-    VIM_CONTENTS="$(sed -n -E 's/^\/\* !bash//p;s/^(\/\*| \*| \*\/)//p' <<< "$VIM_CONTENTS")"
-    sed -n -E '/^(\/\*| \*| \*\/)/p' <<< "$VIM_CONTENTS_RAW"
+extract_comment () {
+    local first
+    IFS= read -r first || return
+    case "$first" in
+        \#!*)
+            echo "${first:2}"
+            sed -E -n 's,# ?,,p'
+            ;;
+        \#\ !*)
+            echo "${first:3}"
+            sed -E -n 's,# ?,,p'
+            ;;
+        //!*)
+            echo "${first:3}"
+            sed -E -n 's,// ?,,p'
+            ;;
+        //\ !*)
+            echo "${first:4}"
+            sed -E -n 's,// ?,,p'
+            ;;
+        /\*!*)
+            echo "${first:3}"
+            sed -E -n 's,^(/\*| \*| \*/),,p'
+            ;;
+        /\*\ !*)
+            echo "${first:4}"
+            sed -E -n 's,^(/\*| \*| \*/),,p'
+            ;;
+        *)
+            return 1
+    esac
+}
+
+print_comment () {
+    local first
+    IFS= read -r first || return
+    echo "$first"
+    case "$first" in
+        \#!*)
+            sed -E -n '/^# ?/p'
+            ;;
+        \#\ !*)
+            sed -E -n '/^# ?/p'
+            ;;
+        //!*)
+            sed -E -n '/^\/\/ ?/p'
+            ;;
+        //\ !*)
+            sed -E -n '/^\/\/ ?/p'
+            ;;
+        /\*!*)
+            sed -E -n '/^(\/\*| \*| \*\/)/p'
+            ;;
+        /\*\ !*)
+            sed -E -n '/^(\/\*| \*| \*\/)/p'
+            ;;
+    esac
+}
+
+execute () {
+    local first
+    IFS= read -r first || return
+    cat | bash -c "$first"
+}
+
+if comment="$(extract_comment <<< "$VIM_CONTENTS")"; then
+    print_comment <<< "$VIM_CONTENTS"
+    execute <<< "$comment"
+else
+    bash <<< "$VIM_CONTENTS"
 fi
-
-bash <<< "$VIM_CONTENTS"
